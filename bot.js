a2330558e714856401ace4daf8a5e5293cad04c2
Ale <cxiesestro@gmail.com>
2019-12-05 11:51:50 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    https://testtoshowskills.000webhostapp.com/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.linkedin.com/in/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var selected;
+
+    selected = document.querySelector(".pv-s-profile-actions");
+    selected.click();
+    selected = document.querySelector("[aria-label='Add a note']");
+    selected.click();
+
+    selected = document.querySelector('textarea[name="message"]')
+    selected.value = 'message.';
+})();
