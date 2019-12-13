101478a5f5fd83b30e260cfba0819ec1cd28af2f
Ale <cxiesestro@gmail.com>
2019-12-12 20:29:29 -0400

    change cookies to localStorage

--- bot.js
+++ bot.js
@@ -11,28 +11,6 @@
 (function() {
     'use strict';
     
-    function setCookie(cname, cvalue, exdays) {
-        var d = new Date();
-        d.setTime(d.getTime() + (exdays*24*60*60*1000));
-        var expires = "expires="+ d.toUTCString();
-        document.cookie = cname + "=" + cvalue + ";" + expires;
-    }
-
-    function getCookie(cname) {
-        var name = cname + "=";
-        var decodedCookie = decodeURIComponent(document.cookie);
-        var ca = decodedCookie.split(';');
-        for(var i = 0; i <ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-            }
-        }
-        return "";
-    }
     var selected;
 
     selected = document.querySelector(".pv-s-profile-actions");
@@ -50,8 +28,8 @@
     
     selected.click();
     
-    var i = getCookie('counter');
-    setCookie('counter', i + 1, 200);
+    var i = localStorage.counter;
+    localStorage.counter -= -1;
 
-    window.location.href = "https://www.linkedin.com/in/" + getCookie('links').split(",")[i];
+    window.location.href = "https://www.linkedin.com/in/" + localStorage.links.split(",")[i] + "/";
 })();
